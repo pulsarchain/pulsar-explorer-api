e8b42c115ca2aa31abe176ae0f2186c8bb68bdbf
liuwenzhong <15021917166@163.com>
2020-08-26 15:15:41 +0800

    init pulsar explorer

--- config/config.local.js
+++ config/config.local.js
@@ -8,8 +8,8 @@
 // 159.138.35.94
 // 159.138.35.94
 exports.mongoose = {
-  url: "mongodb://8.210.31.94:27017/bosha_explorer_dev",
+  url: "mongodb://xxxxxx:27017/bosha_explorer_dev",
 };
 
-exports.httpProvider = "http://8.210.31.94:8545";
+exports.httpProvider = "http://xxxxxxx:8545";
 exports.noticeUrl = "http://xxxxx";
